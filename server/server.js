f380c43bda045c2bed0189f2dee3cecc455e8d73
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2025-04-22 22:48:05 -0400

    Add basic Express server configuration

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/api/test', (req, res) =>{
+    res.json({message: 'Backend is working!'});
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
