1f672741e998eb4ede7af32f0499982a7ee6631a
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2025-04-23 17:15:10 -0400

    Fix Tailwind CSS dependencies and PostCSS configuration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import axios from 'axios';
 
 const App = () => {
+    const [Message, setMessage] = useState('');
+    const [Loading, setLoading] = useState(true);
+
+    useEffect(() =>{
+        const fetchData = async() =>{
+            try{
+                const response = await axios.get('http://localhost:5000/api/test');
+                setMessage(response.data.Message);
+            } catch(error){
+                setMessage('Error connecting to the backend!');
+                console.log('Error fetching data: ', error)
+            }
+            finally{
+                setLoading(false);
+            }
+        }
+        fetchData();
+    }, [])
   return (
-    <div>
-      <h1 className='text-3xl font-bold underline'>
-        Hello World
-      </h1>
+
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+        <div className="bg-white p-8 rounded-lg shadow-md max-w-md w-full">
+        <h1 className="text-2xl font-bold text-center text-blue-800 mb-6">
+            ProTrack
+        </h1>
+        </div>
     </div>
   )
 }
